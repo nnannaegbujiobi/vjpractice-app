60601f73b0c7fab80f77a70f5f5f249560405133
nnannaegbujiobi <nnanna@newvybe.com>
2018-10-17 15:14:50 -0500

    adding axios request

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,18 @@
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
-    <h1>{{secondMessage}}</h1>
-    <li v-for="task in tasks">{{task.id}}: {{task.text}} : {{task.complete}}</li>
+    <div>
+      <h5>total tasks: {{tasks.length}}</h5>
+      <div>
+    <button v-on:click="removeCompletedTasks()">Clear Completed Tasks</button>
+  </div>
+    </div>
+    
+    <li v-for="task in tasks">{{task.id}}: {{task.text}} : {{task.complete}}
+    <h3 v-on:click="toggleComplete(task)">{{task.text}}</h3>
+    <h4 v-bind:clase ="{strike: !task.text}"></h4>
+
+    </li>
 
     <h2> Add Task:</h2>
   
@@ -20,53 +29,73 @@
 </template>
 
 <style>
+  .strike {
+    text-decoration: line-through;
+  }
 </style>
 
 <script>
+  var axios = require("axios");
 export default {
-  data: function() {
-    return {
-      message: "Welcome to Vue.js!",
-      secondMessage: "To do list",
-      tasks: [
-        {
-          id:1,
-          text: "Take out the garbage",
-          complete: "not completed"
-
-
-        },
-        {
-          id:2,
-          text:"clean the house",
-          complete:"completed"
-
-        },
-        {
-          id:3,
-          text:"pick up the kids",
-          complete:"completed"
-        }
-
-      ],
-      newTask: {id:"",text:"",complete:""}
-
-
-    };
-  },
-  created: function() {},
-  methods: {
-    addTask: function() {
-      if (this.newTask.text) {
+    data: function() {
+      return { 
+        tasks: [],
+        newTask: {id:"",text:"",complete:true}
+   
+      };
+    },
+    created: function() {
+      axios.get("http://localhost:3000/tasks").then(function(response) {
+        console.log(response.data);
+        this.tasks = response.data;
+      }.bind(this));
+    },
+    methods: {
+      addTask: function() {
         this.tasks.push(this.newTask);
-        this.newTask.text = "",
-        this.newTask = {id:"",text:"",complete:""};
-      } else {
-        this.error = "you must enter text!"
+        var params = {
+          name: this.newTask.text,
+          complete: this.newTask.complete 
+        };
+        axios.post("http://localhost:3000/api/tasks",params).then(function(response) {
+          this.tasks.push(response.data);
+          this.newTask.text = "";
+          this.newTask.complete = "";
+        }.bind(this));
       }
-    }
-  },
-  computed: {}
-};
 
+
+  
+
+
+
+
+
+    // addTask: function() {
+    //     if (this.newTask.text) {
+    //       this.tasks.push(this.newTask);
+    //       this.newTask.text = "",
+    //       this.newTask = {id:"",text:"",complete:""};
+    //     } else {
+    //       this.error = "you must enter text!";
+    //     }
+    },
+    toggleComplete: function(task) {
+      task.complete = !task.complete;
+    },
+
+    numberOfIncompleteTasks: function() {
+      var count = 0;
+      for (var i = 0; i < this.tasks.length; i++) {
+        if (!this.tasks[i].completed) {
+          count += 1;
+        }
+      }
+      return count;
+  
+    
+    },
+    computed: {}
+  
+};
 </script>
